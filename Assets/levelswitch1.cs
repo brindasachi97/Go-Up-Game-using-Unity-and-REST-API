913b883f501871cb947fc276d78a57cdea3c7bd6
brindasachi97 <brindavana.sachidanand@gmail.com>
2020-01-08 14:35:04 -0800

    Add files via upload

---
+++ Assets/levelswitch1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelswitch1 : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+    private Score time = new Score();
+
+    void Start()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //load level with build index
+            time.AddScore(time.GetLocalTime());
+            time.SetLocalTime(300);
+
+            SceneManager.LoadScene(index);
+
+            //
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
